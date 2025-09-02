2ac17c50871f105d56e29c442dfc1a891692350b
harshit Goyal <harshitgo0409@gmail.com>
2025-09-02 13:05:34 +0530

    Refactor map function syntax in SuitsPage component for improved readability.

--- Devs14/src/components/SuitsPage.jsx
+++ Devs14/src/components/SuitsPage.jsx
@@ -172,14 +172,14 @@ function SuitsPage() {
                 </h3>
                 <div className="flex gap-3 overflow-x-auto">
                   {
-                    styleOptions[selectedStyle].map((option,index)=>{
+                    styleOptions[selectedStyle].map((option,index)=>(
                         <div
                         key={index}
                         className={`w-16 h-16 ${option.color} rounded-lg border-2 ${option.borderColor} flex-shrink-0 flex items-center justify-center text-xs text-center font-medium p-1`}
                       >
                         {option.name}
                       </div>
-                    })
+                    ))
                   }
                 </div>
               </div>
